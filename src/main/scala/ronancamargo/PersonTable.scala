320c0c931c86cbd43df9f38e7026e67d05d6f835
RonanCamargo <91966367+RonanCamargoDaruma@users.noreply.github.com>
2022-01-07 11:34:13 -0300

    Add queries

--- src/main/scala/ronancamargo/PersonTable.scala
+++ src/main/scala/ronancamargo/PersonTable.scala
@@ -1,5 +1,13 @@
 package ronancamargo
 
-class PersonTable {
-  
+import slick.jdbc.PostgresProfile.api._
+
+case class Person(id: Int, firstName: String)
+
+class PersonTable(tag: Tag) extends Table[Person](tag, "person") {
+
+  def id    = column[Int]("id")
+  def name2 = column[String]("name")
+
+  override def * = (id, name2) <> (Person.tupled, Person.unapply)
 }
